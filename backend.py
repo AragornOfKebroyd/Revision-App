2f191ee32fbb5de1eb370eb00885d9f00c190707
AragornOfKebroyd <86985645+AragornOfKebroyd@users.noreply.github.com>
2022-01-14 14:59:16 +0000

    Add files via upload

--- backend.py
+++ backend.py
@@ -1,12 +1,28 @@
+#imports
+import sqlite3
+#constants
+questionTypes = ["MultiChoice","SingleChoice"]
+
+def CreateQuestion(questionInput,answerInput,questionType):
+    #Validation
+    if not isinstance(questionInput, (str)):
+        return "Error 69"
+    if not isinstance(answerInput, (str)):
+        return "Error 70"
+    if not questionType in questionTypes:
+        return "Error 71"
 
 
-
-
-
-def main():
+    #open database
+    with sqlite3.connect("Questions.db") as db: cursor=db.cursor()
     
 
 
+    
+def main():
+    print(CreateQuestion("what?","","SingleChoice"))
 
-if __name__ = "__main__":
+
+
+if __name__ == "__main__":
     main()
---
+++ github.pyw
@@ -0,0 +1,7 @@
+import webbrowser
+
+def main(): #openes github repository
+    webbrowser.open("https://github.com/AragornOfKebroyd/Revision-App/upload/main")
+    
+if __name__ == "__main__":
+    main()
